6feaf32f46e66265086338c601092c104e91b726
atia mobasshera <atia.codesmith@gmail.com>
2019-07-23 17:39:48 +0600

    threading added

--- venv/Scraping.py
+++ venv/Scraping.py
@@ -10,12 +10,24 @@ from xml.etree import ElementTree
 from lxml import html
 import re
 from datetime import datetime
+from fake_useragent import UserAgent
+from urllib.error import HTTPError
+from urllib.error import URLError
+import threading
 
 
 if __name__ == '__main__':
-    while True:
-        website_url = requests.get('https://www.dsebd.org/latest_share_price_scroll_by_change.php').text
-        soup = BeautifulSoup(website_url, 'lxml')
+    # while True:
+    def run_check():
+        threading.Timer(55.0, run_check).start()
+        ua = UserAgent()
+        header = {'user-agent': ua.chrome}
+        url = 'https://www.dsebd.org/latest_share_price_scroll_by_change.php'
+        try:
+            page = requests.get(url, headers=header).text
+        except requests.exceptions.RequestException:
+            time.sleep(300)
+        soup = BeautifulSoup(page, 'lxml')
         time.sleep(15)
         s = soup.findAll('table')[5]
         a = len(s.findAll('table'))
@@ -36,9 +48,10 @@ if __name__ == '__main__':
             # print(tbl6)
             final_data = [[],
                           [datetime.now(), tbl2, tbl3, tbl5, tbl6]]
-            myfile = open('F:\data_saving\wednesday.csv', 'a+', newline='')
+            myfile = open('F:\data_saving\Sunday21-7-19.csv', 'a+', newline='')
             with myfile:
                 writer = csv.writer(myfile)
                 writer.writerows(final_data)
 
-    time.sleep(40)
+    # time.sleep(40)
+    run_check()
